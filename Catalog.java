5ba000fb0971cd0bcdb680878e1e8e204f87606d
shimshiam <bmshiam05@gmail.com>
2026-02-11 22:23:31 -0500

    added catalog

---
+++ Catalog.java
@@ -0,0 +1,15 @@
+public class Catalog{
+    
+    private String name;
+    private ArrayList<Item> list;
+
+    // Constructor for Catalog
+    public Catalog(String name){
+        this.name = name;
+        list = new ArrayList<Item>();
+    }
+    
+
+        
+
+}
