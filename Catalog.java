57d8c42fe525468c87c3e0dbda3cad513041adbd
shimshiam <bmshiam05@gmail.com>
2026-02-11 23:11:32 -0500

    Added constructor and return calls for add, size, get, and getName

--- Catalog.java
+++ Catalog.java
@@ -1,15 +1,29 @@
-public class Catalog{
+import java.util.*;
+
+public class Catalog {
     
     private String name;
     private ArrayList<Item> list;
 
-    // Constructor for Catalog
     public Catalog(String name){
         this.name = name;
-        list = new ArrayList<Item>();
+        this.list = new ArrayList<Item>();
     }
     
+    public void add(Item item){ // Adds the item to the catalog
+        list.add(item);
+    }
 
-        
+    public int size(){ // Returns the size of the catalog
+        return list.size();
+    }
 
+    public Item get(int index){ // Returns the item at the given index in the catalog
+        list.get(index);
+        return list.get(index);
+    }
+
+    public String getName(){ // Returns the name of the catalog
+        return name;
+    }
 }
